3b8ba4bc5a170695b8813b2ff57f7b7841637054
Finisus <finisvisus@gmail.com>
2024-03-22 01:53:26 +0530

    fix final audio

--- app.js
+++ app.js
@@ -5,12 +5,21 @@ document.addEventListener("DOMContentLoaded", function() {
   const audio = new Audio('./metadata/the-godfather-theme-song.mp3');
   audio.loop = true;
   audio.volume = 0.69;
+  const playBtn = document.getElementById('play-music');
   const muteBtn = document.getElementById('mute-music');
   const unmuteBtn = document.getElementById('unmute-music');
 
-  // show mute button by default
-  muteBtn.style.display = 'block';
+  // show play button by default
+  playBtn.style.display = 'block';
+  muteBtn.style.display = 'none';
   unmuteBtn.style.display = 'none';
+
+  // Play audio on play button click
+  playBtn.addEventListener('click', function() {
+    audio.play();
+    playBtn.style.display = 'none';
+    muteBtn.style.display = 'block';
+  });
 
   // mute audio
   muteBtn.addEventListener('click', function() {
@@ -19,15 +28,12 @@ document.addEventListener("DOMContentLoaded", function() {
     unmuteBtn.style.display = 'block';
   });
 
-  //unmute audio
+  // unmute audio
   unmuteBtn.addEventListener('click', function() {
     audio.muted = false;
     muteBtn.style.display = 'block';
     unmuteBtn.style.display = 'none';
   });
-
-  // play audio on page load
-  audio.play();
 
   /* Card-switch b/w main-tokenomics */
   const mainCard = document.getElementById('main');
@@ -40,6 +46,5 @@ document.addEventListener("DOMContentLoaded", function() {
     tokenomics.style.display = tokenomics.style.display === 'none' ? 'flex' : 'none';
     switchCard.textContent = tokenomics.style.display === 'none' ? 'Tokenomics' : 'Go back';
   });
-
 
 });
